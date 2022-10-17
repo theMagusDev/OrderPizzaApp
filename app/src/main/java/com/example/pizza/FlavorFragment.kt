0518e51bc1be9b10601f5547687c07472ae87dd6
Magus Dev <114507764+theMagusDev@users.noreply.github.com>
2022-10-17 21:03:44 +0300

    Create and setup Start- and FlavorFragment and their layouts, create nav_graph, update activity_main.xml and MainActivity, add dependencies and view- & databinding, create dimens.xml, etc.

---
+++ app/src/main/java/com/example/pizza/FlavorFragment.kt
@@ -0,0 +1,34 @@
+package com.example.pizza
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pizza.databinding.FragmentFlavorBinding
+
+class FlavorFragment : Fragment() {
+    // Binding object instance corresponding to the fragment_start.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment.
+    private var binding: FragmentFlavorBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentFlavorBinding = FragmentFlavorBinding.inflate(inflater, container, false)
+        binding = fragmentFlavorBinding
+        return fragmentFlavorBinding.root
+    }
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
---
+++ app/src/main/java/com/example/pizza/StartFragment.kt
@@ -0,0 +1,47 @@
+package com.example.pizza
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.pizza.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+
+    // Binding object instance corresponding to the fragment_start.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment.
+    private var binding: FragmentStartBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentStartBinding = FragmentStartBinding.inflate(inflater, container, false)
+        binding = fragmentStartBinding
+        return fragmentStartBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.startFragment = this
+    }
+
+    fun orderPizza() {
+        findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
+        Log.d("StartFragment", "Navigated successfully")
+    }
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
