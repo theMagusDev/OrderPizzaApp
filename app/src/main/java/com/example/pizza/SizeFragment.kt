0919de8d44005137844b66d7fca3ac9261fcee6c
Magus Dev <114507764+theMagusDev@users.noreply.github.com>
2022-10-19 20:38:11 +0300

    SizeFragment setup, fragment_size setup, navGraph update

--- app/src/main/java/com/example/pizza/SizeFragment.kt
+++ app/src/main/java/com/example/pizza/SizeFragment.kt
@@ -1,7 +1,40 @@
 package com.example.pizza
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.pizza.databinding.FragmentSizeBinding
+import com.example.pizza.databinding.FragmentStartBinding
 
 class SizeFragment : Fragment() {
+    // Binding object instance corresponding to the fragment_start.xml layout
+    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
+    // when the view hierarchy is attached to the fragment.
+    private var binding: FragmentSizeBinding? = null
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentSizeBinding = FragmentSizeBinding.inflate(inflater, container, false)
+        binding = fragmentSizeBinding
+        return fragmentSizeBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.sizeFragment = this
+    }
+
+    /**
+     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
+     * is being removed. As a result, clear out the binding object.
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
 }
