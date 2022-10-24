29dbf32092c01c6c5698143a6ae27be332e4f148
Magus Dev <114507764+theMagusDev@users.noreply.github.com>
2022-10-24 19:51:02 +0300

    Create FragmentSummary and its layout

---
+++ app/src/main/java/com/example/pizza/ui/order/SummaryFragment.kt
@@ -0,0 +1,27 @@
+package com.example.pizza.ui.order
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pizza.databinding.FragmentSummaryBinding
+
+class SummaryFragment : Fragment() {
+    private var binding: FragmentSummaryBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val summaryFragmentBinding = FragmentSummaryBinding.inflate(inflater, container, false)
+        binding = summaryFragmentBinding
+        return summaryFragmentBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
